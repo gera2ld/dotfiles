3184fc555d36c882a752885d6c28f7662e0be496
Gerald <code@gera2ld.space>
2023-11-16 16:20:59 +0800

    fix: show location list on search

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,3 +1,5 @@
+local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 local silent = { silent = true, noremap = true }
 
 -- CTRL-U in insert mode deletes a lot.  Use CTRL-G u to first break undo,
@@ -5,8 +7,20 @@ local silent = { silent = true, noremap = true }
 vim.keymap.set('i', '<c-u>', '<c-g>u<c-u>', silent)
 
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<cr>', silent)
+
 vim.keymap.set('n', '<leader>fg', ':silent lgrep<space>')
-vim.keymap.set('n', 'g*', ":silent lgrep <c-r>=expand('<cword>')<cr> | lopen<cr>")
+vim.keymap.set('n', 'g*', ":silent lgrep <c-r>=expand('<cword>')<cr><cr>")
+augroup('QuickfixWindows', {})
+autocmd("QuickFixCmdPost", {
+  group='QuickfixWindows',
+  pattern='[^l]*',
+  command='cwindow',
+})
+autocmd("QuickFixCmdPost", {
+  group='QuickfixWindows',
+  pattern='l*',
+  command='lwindow',
+})
 
 vim.keymap.set('n', '<leader>h', '<cmd>HopWord<cr>', silent)
 
