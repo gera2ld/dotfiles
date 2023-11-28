72a2a1be5d29954a389d142855a904c2104094cd
Gerald <code@gera2ld.space>
2023-11-28 14:31:05 +0800

    fix: update neo-tree config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -99,6 +99,13 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
       window = {
         mappings = {
           ["-"] = function(state)
