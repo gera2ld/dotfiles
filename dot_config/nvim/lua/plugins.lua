6d1e4afdbd48774e17f4c1ab8a545f96e6df4ace
Gerald <code@gera2ld.space>
2024-01-08 17:28:18 +0800

    fix: add textcase

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -308,4 +308,15 @@ return {
     },
     event = 'VeryLazy',
   },
+  {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Telescope" },
+    },
+  }
 }
