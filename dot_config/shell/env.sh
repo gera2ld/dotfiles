5ba76330f82e033fcc8c580d17cd316bffa5fa57
Gerald <code@gera2ld.space>
2026-09-25 23:50:59 +0900

    update

--- dot_config/shell/env.sh
+++ dot_config/shell/env.sh
@@ -1,3 +1,6 @@
+__SHELL="${ZSH_VERSION:+zsh}"
+__SHELL="${__SHELL:-${BASH_VERSION:+bash}}"
+
 # Shell {{{
 __add_path() {
 	case ":$PATH:" in
@@ -11,6 +14,7 @@ __add_path() {
 	export PATH
 }
 __add_path "$HOME/.local/bin" 1
+set -o vi
 # }}}
 
 # Locale {{{
@@ -20,15 +24,17 @@ export LANG="en_US.UTF-8"
 
 # Editor {{{
 export EDITOR="nvim"
+alias vi=nvim
 # }}}
 
 # Mise {{{
 alias m=mise
 alias mx='mise exec --'
-eval "$(mise activate zsh --shims)"
+[ -n "$__SHELL" ] && eval "$(mise activate $__SHELL --shims)"
 # }}}
 
 # Node {{{
+export NODE_USE_ENV_PROXY=1
 export PNPM_HOME=~/.local/share/pnpm
 __add_path "$PNPM_HOME/bin"
 alias p='mise exec -- pnpm'
@@ -42,4 +48,19 @@ export PIPX_DEFAULT_PYTHON="$HOME/.local/share/mise/shims/python"
 export GOPATH=~/go
 export GOBIN=$GOPATH/bin
 __add_path "$GOBIN"
+# }}}
+
+# tmux {{{
+alias tmux='tmux -2'
+# }}}
+
+# FZF {{{
+export FZF_DEFAULT_COMMAND="rg --files --hidden --follow -g \"!.git\""
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+export FZF_CTRL_T_OPTS="--info=inline --preview='bat -r :100 {}'"
+[ -n "$__SHELL" ] && eval "$(fzf --$__SHELL)"
+# }}}
+
+# Zoxide {{{
+[ -n "$__SHELL" ] && eval "$(mise exec -- zoxide init $__SHELL)"
 # }}}
